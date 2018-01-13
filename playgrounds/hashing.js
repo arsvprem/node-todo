a331204bfe425f51bd6abdca9f8ca6a5fc3c825c
prem <prem@prem.com>
2018-01-13 11:16:34 -0500

    hash user password before save

--- playgrounds/hashing.js
+++ playgrounds/hashing.js
@@ -1,17 +1,32 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-const data = {
-  id: 5
-}
+const password = 'abc123';
 
-const token = jwt.sign(data, 'abc123');
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
 
-console.log(token);
+const hashedPassword = '$2a$10$XyP3ehZBrRCIWtW.Y1M.Y./7/aRoULdiasKMZL5N9AOgMG4eewowq';
 
-const decoded = jwt.verify(token, 'abc123');
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
 
-console.log('decoded', decoded);
+// const data = {
+//   id: 5
+// }
+
+// const token = jwt.sign(data, 'abc123');
+
+// console.log(token);
+
+// const decoded = jwt.verify(token, 'abc123');
+
+// console.log('decoded', decoded);
 
 // const message = 'This is a sample message';
 // const hash = SHA256(message).toString();
